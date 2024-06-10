8d389433ae94783e801c4d5b8f82c24c3bedc6f6
filippogenesini <78311187+filippogenesini@users.noreply.github.com>
2024-06-10 22:39:15 +0200

    Home

---
+++ HomeDynamics.js
@@ -0,0 +1,31 @@
+
+console.log("ciao mondo");
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const textPORT = document.querySelector('.text-PORT');
+    const textNAME = document.querySelector('.text-NAME');
+    const menu = document.querySelector('.menu');
+
+    // Aggiungi un event listener per rilevare quando la prima animazione termina
+    textPORT.addEventListener('animationend', (event) => {
+        if (event.animationName === 'typing-1') {
+
+            menu.style="transition-duration: 10s; display: flex;";
+
+            // Aggiunge un ritardo prima di iniziare la seconda animazione
+            setTimeout(() => {
+                textNAME.classList.add('show-NAME');
+            }, 5); // Ritardo di 1 secondo
+        }
+    });
+
+    
+
+    // Aggiungi un event listener per avviare l'animazione di digitazione della seconda riga
+    textNAME.addEventListener('animationstart', () => {
+        textPORT.style= 'border: none;';
+        // Imposta l'animazione con il ritardo desiderato
+        textNAME.style.animationDelay = '0s';
+    });
+});
